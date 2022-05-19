409e3e4decffa24d7026533aaffad583f78506a2
maxschemann <102051571+maxschemann@users.noreply.github.com>
2022-05-19 16:01:19 +0200

    Update backend/src/main/java/de/neuefische/backend/service/IndexCardService.java
    Co-authored-by: MariaKohnen <102051473+MariaKohnen@users.noreply.github.com>

--- backend/src/main/java/de/neuefische/backend/service/IndexCardService.java
+++ backend/src/main/java/de/neuefische/backend/service/IndexCardService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Builder
 public class IndexCardService {
 
     private final IndexCardRepository indexCardRepo;
