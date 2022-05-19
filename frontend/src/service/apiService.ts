4c3b3c21427102883135d848270a3d3d17f0d1a9
maxschemann <max.schemann@web.de>
2022-05-19 18:42:26 +0200

    corrected path, missing "/" addNewIndexCard working

--- frontend/src/service/apiService.ts
+++ frontend/src/service/apiService.ts
@@ -2,7 +2,7 @@ import {IndexCard} from "../model/IndexCard";
 import axios from "axios";
 
 export const postIndexCard: (indexCard: Omit<IndexCard, "id">) => Promise<IndexCard> = (indexCard) => {
-    return axios.post("api/indexcard", indexCard)
+    return axios.post("/api/indexcard", indexCard)
         .then(response => response.data)
 }
 
