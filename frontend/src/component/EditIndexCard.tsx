633fa3f075174dcb45243ad18721851defbbdac0
maxschemann <max.schemann@web.de>
2022-05-20 16:33:43 +0200

    Mui props in cardTheme ausgelagert

--- frontend/src/component/EditIndexCard.tsx
+++ frontend/src/component/EditIndexCard.tsx
@@ -1,4 +1,4 @@
-import {Card, CardContent, Fab, TextField} from "@mui/material";
+import {Card, CardContent, Fab, TextField, ThemeProvider} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
@@ -7,6 +7,7 @@ import {FormEvent, useState} from "react";
 import {IndexCard} from "../model/IndexCard";
 import {toast} from "react-toastify";
 import '../stylesheets/EditIndexCard.css'
+import {cardTheme} from "../stylesheets/themes";
 
 type EditIndexCardProps = {
     addNewIndexCard: (newIndexCard: Omit<IndexCard, "id">) => void
@@ -38,51 +39,37 @@ export default function EditIndexCard({addNewIndexCard}: EditIndexCardProps) {
         addNewIndexCard(newIndexCard)
     }
 
-    return <div id={"editIndexCard"}>
-        <Card>
-            <CardContent>
-                <form onSubmit={submitIndexCard}>
-                    <div id={"topRow"}>
-                        <TextField label={"Origin"}
-                                   variant={"outlined"}
-                                   value={term1}
-                                   placeholder={"Enter a word..."}
-                                   multiline
-                                   sx={{padding: '5px'}}
-                                   onChange={event => updateTerm(1, event.target.value)}/>
-                        <TextField label={"Target"}
-                                   variant={"outlined"}
-                                   value={term2}
-                                   placeholder={"Enter a translation.."}
-                                   multiline
-                                   sx={{padding: '5px'}}
-                                   onChange={event => updateTerm(2, event.target.value)}/>
-                        <Fab type={"submit"}
-                            size={"small"}
-                            sx={{marginTop: '10px',
-                                marginLeft:'5px'}}>
-                            <AddIcon/>
-                        </Fab>
-                    </div>
-                    <div id={"bottomRow"}>
-                    <Fab size={"medium"}
-                         sx={{margin: '10px',
-                             backgroundColor: '#07bc0c'}}>
-                        <SentimentSatisfiedAltIcon/>
-                    </Fab>
-                    <Fab size={"medium"}
-                         sx={{margin: '10px',
-                             backgroundColor: '#f1c40f'}}>
-                        <SentimentSatisfiedIcon/>
-                    </Fab>
-                    <Fab size={"medium"}
-                         sx={{margin: '10px',
-                             backgroundColor: '#e74c3c'}}>
-                        <SentimentDissatisfiedIcon/>
-                    </Fab>
-                    </div>
-                </form>
-            </CardContent>
-        </Card>
-    </div>
+    return (<div id={"editIndexCard"}>
+            <ThemeProvider theme={cardTheme}>
+                <Card>
+                    <CardContent>
+                        <form onSubmit={submitIndexCard}>
+                            <div id={"topRow"}>
+                                <TextField value={term1}
+                                           placeholder={"Enter a word..."}
+                                           onChange={event => updateTerm(1, event.target.value)}/>
+                                <TextField value={term2}
+                                           placeholder={"Enter a translation.."}
+                                           onChange={event => updateTerm(2, event.target.value)}/>
+                                <Fab type={"submit"}>
+                                    <AddIcon/>
+                                </Fab>
+                            </div>
+                            <div id={"bottomRow"}>
+                                <Fab sx={{backgroundColor: '#07bc0c'}}>
+                                    <SentimentSatisfiedAltIcon/>
+                                </Fab>
+                                <Fab sx={{backgroundColor: '#f1c40f'}}>
+                                    <SentimentSatisfiedIcon/>
+                                </Fab>
+                                <Fab sx={{backgroundColor: '#e74c3c'}}>
+                                    <SentimentDissatisfiedIcon/>
+                                </Fab>
+                            </div>
+                        </form>
+                    </CardContent>
+                </Card>
+            </ThemeProvider>
+        </div>
+    );
 }
