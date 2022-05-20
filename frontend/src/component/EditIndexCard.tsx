615f5ba00cfa0beee9cc0678dc2b7115fb3f992d
maxschemann <max.schemann@web.de>
2022-05-20 14:14:51 +0200

    added mui Card and buttons to EditIndexCard.tsx

--- frontend/src/component/EditIndexCard.tsx
+++ frontend/src/component/EditIndexCard.tsx
@@ -1,4 +1,8 @@
-import {Icon, IconButton, TextField} from "@mui/material";
+import {Card, CardContent, Fab, TextField} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
 import {FormEvent, useState} from "react";
 import {IndexCard} from "../model/IndexCard";
 import {toast} from "react-toastify";
@@ -35,23 +39,34 @@ export default function EditIndexCard({addNewIndexCard}: EditIndexCardProps) {
     }
 
     return <div id={"editIndexCard"}>
-
-        <form onSubmit={submitIndexCard}>
-            <TextField label={"Origin"}
-                       variant={"outlined"}
-                       value={term1}
-                       placeholder={"Enter a word..."}
-                       multiline
-                       onChange={event => updateTerm(1, event.target.value)}/>
-            <TextField label={"Target"}
-                       variant={"outlined"}
-                       value={term2}
-                       placeholder={"Enter a translation.."}
-                       multiline
-                       onChange={event => updateTerm(2, event.target.value)}/>
-            <IconButton type={"submit"}>
-                <Icon>add_circle</Icon>
-            </IconButton>
-        </form>
+        <Card>
+            <CardContent>
+                <form onSubmit={submitIndexCard}>
+                    <div>
+                        <TextField label={"Origin"}
+                                   variant={"outlined"}
+                                   value={term1}
+                                   placeholder={"Enter a word..."}
+                                   multiline
+                                   onChange={event => updateTerm(1, event.target.value)}/>
+                        <TextField label={"Target"}
+                                   variant={"outlined"}
+                                   value={term2}
+                                   placeholder={"Enter a translation.."}
+                                   multiline
+                                   onChange={event => updateTerm(2, event.target.value)}/>
+                        <Fab type={"submit"}
+                            size={"small"}>
+                            <AddIcon/>
+                        </Fab>
+                    </div>
+                    <div>
+                    <Fab size={"medium"}><SentimentSatisfiedAltIcon/></Fab>
+                    <Fab size={"medium"}><SentimentSatisfiedIcon/></Fab>
+                    <Fab size={"medium"}><SentimentDissatisfiedIcon/></Fab>
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
     </div>
 }
