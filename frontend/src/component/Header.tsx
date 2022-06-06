97f446a39f34b0381b5cb61ede1bfe939fd147a3
maxschemann <102051571+maxschemann@users.noreply.github.com>
2022-06-06 11:12:56 +0200

    Update frontend/src/component/Header.tsx
    Co-authored-by: TimLucas16 <102078298+TimLucas16@users.noreply.github.com>

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -7,7 +7,7 @@ import React, {useState} from "react";
 import {IndexCard} from "../model/IndexCard";
 import {headerTheme} from "../styles/headerTheme";
 import Box from "@mui/material/Box";
-import '../styles/Header.css'
+import '../styles/Header.css';
 
 type HeaderProps = {
     setIndexCards: (indexCards: IndexCard[]) => void
